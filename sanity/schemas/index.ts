63287b07fd8564cda38435fc747c5a646046e6a6
kasheeumar <kshfkhl@gmail.com>
2023-12-04 02:00:52 +0500

    :fire: Added sanity configurations

---
+++ sanity/schemas/index.ts
@@ -0,0 +1,6 @@
+import resourse from "./resourse.schema";
+import resourcePlaylist from "./resourse-playlist.schema";
+
+const schemas = [resourse, resourcePlaylist];
+
+export default schemas;
--- sanity/schemas/resourse.schema.ts
+++ sanity/schemas/resourse.schema.ts
@@ -48,3 +48,5 @@ const schema = {
     },
   ],
 };
+
+export default schema;
