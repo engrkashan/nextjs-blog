e2b327ffedbc5c1094e7f7d387154e1c83034b8f
kasheeumar <kshfkhl@gmail.com>
2023-12-04 01:10:19 +0500

    :fire: Search Filter added

--- components/ui/Footer.tsx
+++ components/ui/Footer.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col">
       <p>Copyright 2023 Kashan pro | All Rights Reserved</p>
       <div className="flex gap-x-9">
         <Link href="/">Terms & Conditions</Link>
